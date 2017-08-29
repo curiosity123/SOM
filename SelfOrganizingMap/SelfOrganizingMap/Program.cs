c5d1e0b17d2fdb582aeaa34da5f9b3d9a0fa8b1a
Łukasz <ukasz@UKASZ-KOMPUTER>
2017-08-29 10:32:35 +0200

    wpf gui

--- SelfOrganizingMap/SelfOrganizingMap/Program.cs
+++ SelfOrganizingMap/SelfOrganizingMap/Program.cs
@@ -13,22 +13,22 @@ namespace SelfOrganizingMap
         {
             Random r = new Random();
 
-            AdachSOM som = new AdachSOM(2, null);
+            AdachSOM som = new AdachSOM(4, null);
 
             List<string> lab = new List<string>();
             List<double[]> dat = new List<double[]>();
 
-            for (int i = 0; i < 25; i++)
+            for (int i = 0; i < 225; i++)
             {
                 lab.Add("test" + i.ToString());
-                dat.Add(new double[] { r.NextDouble(),r.NextDouble() });
+                dat.Add(new double[] { r.NextDouble() });
                 som.Add(lab.Last(), dat.Last());
             }
 
-            som.StartFitting();
-            Thread.Sleep(2000);
-            som.StopFitting();
-            Thread.Sleep(100);
+            som.StartFitting(12);
+            //Thread.Sleep(500);
+         //   som.StopFitting();
+            Thread.Sleep(1000);
             Console.WriteLine("\n");
             for (int i = 0; i < lab.Count; i++)
             {
@@ -38,6 +38,19 @@ namespace SelfOrganizingMap
                     Console.Write(" |" + d + "| ");
                 Console.WriteLine("\n");
             }
+
+
+
+            //for (double i = 0; i < 100; i++)
+            //{
+            //    double d = i / 100;
+            //    Console.ForegroundColor = (ConsoleColor)1 + som.GetGroup(new double[] { d});
+            //    Console.Write("Group: " + som.GetGroup(new double[] { d }) + "   value: " + d.ToString());
+            //   // Console.Write(" |" +  ((double)(i/100)).ToString() );
+            //    Console.WriteLine("\n");
+            //}
+
+
             Console.ReadKey();
         }
     }
@@ -46,12 +59,13 @@ namespace SelfOrganizingMap
     {
         bool isStarted = false;
         public delegate void DiagnosticDatas(string diagString);
-        private event DiagnosticDatas DiagnosticInfo;
+        event DiagnosticDatas DiagnosticInfo;
         List<double[]> Datas;
         List<string> Labels;
         List<double[]> Neurons;
         double[] DistanceMatrix;
         int howManyGroups = 0;
+
 
         public void Add(string _label, double[] _data)
         {
@@ -72,6 +86,7 @@ namespace SelfOrganizingMap
                     Neurons[i][j] = r.NextDouble();
             }
         }
+
         public AdachSOM(int _howManyGroups, DiagnosticDatas _diagnosticDatas = null)
         {
             if (_diagnosticDatas != null)
@@ -102,6 +117,23 @@ namespace SelfOrganizingMap
 
             return group;
         }
+
+        public int GetGroup(string label)
+        {
+            int index = Labels.IndexOf(label);
+            int group = 0;
+            double distance = CalculateDistance(Datas[index], Neurons[0]);
+
+            for (int i = 0; i < Neurons.Count; i++)
+                if (CalculateDistance(Datas[index], Neurons[i]) < distance)
+                {
+                    distance = CalculateDistance(Datas[index], Neurons[i]);
+                    group = i;
+                }
+
+            return group;
+        }
+
         public double GetDistance(double[] data)
         {
             int group = 0;
@@ -117,24 +149,30 @@ namespace SelfOrganizingMap
             return distance;
         }
 
-        public void StartFitting()
+        public void StartFitting(int AutoStopPrecision = 14)
         {
             if (isStarted == false)
             {
+                double result = 0;
                 isStarted = true;
                 Init();
                 new Task(() =>
                 {
                     while (isStarted)
                     {
-                        DiagnosticInfo("Total distance: " + Math.Round(FitNetworkWinner(Datas, Neurons, DistanceMatrix), 4) + "      | ");
-                        // DiagnosticInfo("Total distance: " + Math.Round(FitNetworkGauss(Datas, Neurons, DistanceMatrix), 4) + "      | ");
+                        double tempResult = FitNetworkWinner(Datas, Neurons, DistanceMatrix);
 
-                        // Print(Neurons);
+                        if (AutoStopPrecision != 14 && Math.Round(tempResult, AutoStopPrecision) == Math.Round(result, AutoStopPrecision))
+                            isStarted = false;
+
+                        result = tempResult;
+
+                        DiagnosticInfo("Total distance: " + Math.Round(result, AutoStopPrecision) + "\n");
                     }
                 }).Start();
             }
         }
+
         public void StopFitting()
         {
             isStarted = false;
@@ -163,23 +201,6 @@ namespace SelfOrganizingMap
             totalDistance /= Inputs.Count;
             return totalDistance;
         }
-        private double FitNetworkGauss(double[][] Inputs, double[][] Neurons, double[] DistanceMatrix)
-        {
-            double totalDistance = 0;
-            for (int i = 0; i < Inputs.Length; i++)
-            {
-                for (int j = 0; j < DistanceMatrix.Length; j++)
-                {
-                    DistanceMatrix[j] = CalculateDistance(Inputs[i], Neurons[j]);
-                    int winner = FindShortestDistance(DistanceMatrix);
-                    totalDistance += DistanceMatrix[winner];
-                    FitNeuron(Inputs[i], Neurons[j], CalculateDistance(Inputs[i], Neurons[j]), winner);
-                }
-            }
-            totalDistance /= Inputs.Length;
-            return totalDistance;
-        }
-
 
         private int FindShortestDistance(double[] distanceMatrix)
         {
@@ -198,8 +219,9 @@ namespace SelfOrganizingMap
         private void FitNeuron(double[] Inputs, double[] Neurons)
         {
             for (int i = 0; i < Inputs.Length; i++)
-                Neurons[i] -= 0.1 * (Neurons[i] - Inputs[i]);
+                Neurons[i] -= 0.01 * (Neurons[i] - Inputs[i]);
         }
+
         private void FitNeuron(double[] Inputs, double[] Neurons, double distance, double winner)
         {
             //   if (distance <0.5)
@@ -220,190 +242,6 @@ namespace SelfOrganizingMap
             else
                 throw new Exception("Calculate distance error");
         }
-
-        private void Print(double[][] Neurons)
-        {
-            for (int i = 0; i < Neurons.Length; i++)
-                for (int j = 0; j < Neurons[i].Length; j++)
-                    DiagnosticInfo(Math.Round(Neurons[i][j], 4) + " | ");
-
-            DiagnosticInfo("\n");
-        }
     }
-
-    public class AdachSOMList
-    {
-        bool isStarted = false;
-        public delegate void DiagnosticDatas(string diagString);
-        private event DiagnosticDatas DiagnosticInfo;
-        double[][] Datas;
-        double[][] Neurons;
-        double[] DistanceMatrix;
-
-
-        public AdachSOMList(double[][] _datas, int _howManyGroups, DiagnosticDatas _diagnosticDatas = null)
-        {
-            if (_diagnosticDatas != null)
-                DiagnosticInfo = _diagnosticDatas;
-            else
-                DiagnosticInfo = Console.Write;
-
-            Random r = new Random();
-            Datas = _datas;
-            Neurons = new double[_howManyGroups][];
-            DistanceMatrix = new double[Neurons.Length];
-
-
-            for (int i = 0; i < Neurons.Length; i++)
-            {
-                Neurons[i] = new double[_datas[0].Length];
-                for (int j = 0; j < Datas[0].Length; j++)
-                    Neurons[i][j] = r.NextDouble();
-            }
-
-        }
-
-        public int GetGroup(double[] data)
-        {
-            int group = 0;
-            double distance = CalculateDistance(data, Neurons[0]);
-
-            for (int i = 0; i < Neurons.Length; i++)
-                if (CalculateDistance(data, Neurons[i]) < distance)
-                {
-                    distance = CalculateDistance(data, Neurons[i]);
-                    group = i;
-                }
-
-            return group;
-        }
-        public double GetDistance(double[] data)
-        {
-            int group = 0;
-            double distance = CalculateDistance(data, Neurons[0]);
-
-            for (int i = 0; i < Neurons.Length; i++)
-                if (CalculateDistance(data, Neurons[i]) < distance)
-                {
-                    distance = CalculateDistance(data, Neurons[i]);
-                    group = i;
-                }
-
-            return distance;
-        }
-
-        public void StartFitting()
-        {
-            if (isStarted == false)
-            {
-                isStarted = true;
-                new Task(() =>
-                {
-                    while (isStarted)
-                    {
-                        DiagnosticInfo("Total distance: " + Math.Round(FitNetworkWinner(Datas, Neurons, DistanceMatrix), 4) + "      | ");
-                        // DiagnosticInfo("Total distance: " + Math.Round(FitNetworkGauss(Datas, Neurons, DistanceMatrix), 4) + "      | ");
-
-                        Print(Neurons);
-                    }
-                }).Start();
-            }
-        }
-        public void StopFitting()
-        {
-            isStarted = false;
-        }
-
-        private double FitNetworkWinner(double[][] Inputs, double[][] Neurons, double[] DistanceMatrix)
-        {
-            double totalDistance = 0;
-            List<int> AliveNeurons = new List<int>();
-            for (int i = 0; i < Inputs.Length; i++)
-            {
-                for (int j = 0; j < Neurons.Length; j++)
-                    DistanceMatrix[j] = CalculateDistance(Inputs[i], Neurons[j]);
-
-                int winner = FindShortestDistance(DistanceMatrix);
-                AliveNeurons.Add(winner);
-                totalDistance += DistanceMatrix[winner];
-                FitNeuron(Inputs[i], Neurons[winner]);
-            }
-
-            for (int i = 0; i < Neurons.Length; i++)
-                if (!AliveNeurons.Contains(i))
-                    for (int j = 0; j < Neurons[i].Length; j++)
-                        Neurons[i][j] = 0;
-
-            totalDistance /= Inputs.Length;
-            return totalDistance;
-        }
-        private double FitNetworkGauss(double[][] Inputs, double[][] Neurons, double[] DistanceMatrix)
-        {
-            double totalDistance = 0;
-            for (int i = 0; i < Inputs.Length; i++)
-            {
-                for (int j = 0; j < DistanceMatrix.Length; j++)
-                {
-                    DistanceMatrix[j] = CalculateDistance(Inputs[i], Neurons[j]);
-                    int winner = FindShortestDistance(DistanceMatrix);
-                    totalDistance += DistanceMatrix[winner];
-                    FitNeuron(Inputs[i], Neurons[j], CalculateDistance(Inputs[i], Neurons[j]), winner);
-                }
-            }
-            totalDistance /= Inputs.Length;
-            return totalDistance;
-        }
-
-
-        private int FindShortestDistance(double[] distanceMatrix)
-        {
-            int winner = 0;
-            double distance = 100;
-
-            for (int i = 0; i < distanceMatrix.Length; i++)
-                if (distanceMatrix[i] <= distance)
-                {
-                    distance = distanceMatrix[i];
-                    winner = i;
-                }
-            return winner;
-        }
-
-        private void FitNeuron(double[] Inputs, double[] Neurons)
-        {
-            for (int i = 0; i < Inputs.Length; i++)
-                Neurons[i] -= 0.1 * (Neurons[i] - Inputs[i]);
-        }
-        private void FitNeuron(double[] Inputs, double[] Neurons, double distance, double winner)
-        {
-            //   if (distance <0.5)
-            for (int i = 0; i < Inputs.Length; i++)
-                Neurons[i] -= distance * (0.1 * (Neurons[i] - Inputs[i]));
-        }
-
-        private double CalculateDistance(double[] Input, double[] Neuron)
-        {
-            if (Input.Length == Neuron.Length)
-            {
-                double DifferencBetweenDimensions = 0;
-                for (int i = 0; i < Input.Length; i++)
-                    DifferencBetweenDimensions += Math.Pow(Input[i] - Neuron[i], 2);
-
-                return Math.Sqrt(DifferencBetweenDimensions);
-            }
-            else
-                throw new Exception("Calculate distance error");
-        }
-
-        private void Print(double[][] Neurons)
-        {
-            for (int i = 0; i < Neurons.Length; i++)
-                for (int j = 0; j < Neurons[i].Length; j++)
-                    DiagnosticInfo(Math.Round(Neurons[i][j], 4) + " | ");
-
-            DiagnosticInfo("\n");
-        }
-    }
-
 
 }
---
+++ SelfOrganizingMap/SelfOrganizingMapWpfApp/SOMViewModel.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.Win32;
+using SelfOrganizingMap;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SelfOrganizingMapWpfApp
+{
+    public class SOMViewModel : INotifyPropertyChanged
+    {
+        AdachSOM SOM;
+        DataTable CsvDataTable = new DataTable();
+        List<string> lab = new List<string>();
+        List<double[]> dat = new List<double[]>();
+
+
+        public SOMViewModel()
+        {
+
+
+        }
+        #region Properties
+
+        private int howManyGroups = 4;
+        public int HowManyGroups
+        {
+            get { return howManyGroups; }
+            set
+            {
+                howManyGroups = value;
+                RaisePropertyChangedEvent("HowManyGroups");
+            }
+        }
+
+        public DataTable CsvDataSet
+        {
+            get
+            {
+                return CsvDataTable;
+            }
+            set
+            {
+                CsvDataTable = value;
+                RaisePropertyChangedEvent("CsvDataSet");
+            }
+        }
+
+        #endregion
+
+        #region commands
+
+        public ICommand OpenCommand { get { return new RelayCommand(CanOpen, Open); } }
+        private void Open(object obj)
+        {
+            string Path = "";
+
+            dat = new List<double[]>();
+            lab = new List<string>();
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = Environment.CurrentDirectory;
+            openFileDialog1.Filter = "All Files (*.csv)|*.csv";
+            if (openFileDialog1.ShowDialog() == true)
+            {
+                Path = openFileDialog1.FileName;
+            }
+            else
+                return;
+
+
+
+            string CSVFilePathName = Path;
+            string[] Lines = File.ReadAllLines(CSVFilePathName);
+
+            for (int i = 1; i < Lines.Length; i++)
+            {
+                string[] items = Lines[i].Split(',');
+
+                lab.Add(items[0]);
+
+                double[] temp = new double[items.Length - 1];
+                for (int j = 1; j < items.Length; j++)
+                    temp[j - 1] = double.Parse(items[j].Replace('.', ','));
+                dat.Add(temp);
+            }
+
+
+
+
+
+
+        }
+        public void Normalize(List<double[]> d)
+        {
+           // double min = 1, max = 1;
+            for (int i = 0; i < d[0].Length; i++)
+            {
+                double min = 1, max = 1;
+                for (int j = 0; j < d.Count; j++)
+                {
+                    if (d[j][i] > max)
+                        max = d[j][i];
+
+                    if (d[j][i] < min)
+                        min = d[j][i];
+                }
+
+                for (int j = 0; j < d.Count; j++)
+                    d[j][i] = d[j][i] / max;
+
+
+            }
+            //for (int i = 0; i < d[0].Length; i++)
+            //    for (int j = 0; j < d.Count; j++)
+            //        d[j][i] = d[j][i] / max;
+        }
+
+        private bool CanOpen(object obj)
+        {
+            return true;
+        }
+
+        public ICommand SortCommand { get { return new RelayCommand(CanSort, Sort); } }
+        private void Sort(object obj)
+        {
+            Random r = new Random();
+            SOM = new AdachSOM(HowManyGroups, null);
+            Normalize(dat);
+
+            for (int i = 0; i < lab.Count; i++)
+                SOM.Add(lab[i], dat[i]);
+
+            SOM.StartFitting(10);
+            Thread.Sleep(4000);
+
+
+            CsvDataTable = new DataTable();
+            for (int i = 0; i < HowManyGroups; i++)
+                CsvDataTable.Columns.Add("group " + i.ToString());
+
+
+            for (int i = 0; i < lab.Count; i++)
+            {
+                int group = SOM.GetGroup((lab[i]));
+                double dist = SOM.GetDistance(dat[i]);
+                string[] row = new string[HowManyGroups] ;
+                row[group] = lab[i] + " " +  Math.Round( dist,3).ToString() ;
+                CsvDataTable.Rows.Add(row);
+            }
+
+
+            RaisePropertyChangedEvent("CsvDataSet");
+        }
+        private bool CanSort(object obj)
+        {
+            return true;
+        }
+        #endregion
+      
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+
+
+    public class RelayCommand : ICommand
+    {
+        private Predicate<object> _canExecute;
+        private Action<object> _execute;
+
+        public RelayCommand(Predicate<object> canExecute, Action<object> execute)
+        {
+            this._canExecute = canExecute;
+            this._execute = execute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
